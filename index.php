fb48d9e3ab2632e0e1ebd69b80b810bd9f34b2b2
Stevania Mauren Wanggai <91767848+Stevaniawanggai11@users.noreply.github.com>
2022-03-26 10:20:25 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+</head>
+
+<body>
+
+<?php
+    // This gets something from the database
+    function() {
+        stevania wanggai
+    }
+   /*echo "Hi";
+    echo "Daniel";*/
+?>    
+    
+</body>
+
+</html>
